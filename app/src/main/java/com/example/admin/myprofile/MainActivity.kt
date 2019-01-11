84582cbea179190fa07d1869d3407228343c20a2
Soumodip13 <46412783+Soumodip13@users.noreply.github.com>
2019-01-11 13:41:38 +0530

    Activity Integrated

--- app/src/main/java/com/example/admin/myprofile/MainActivity.kt
+++ app/src/main/java/com/example/admin/myprofile/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.admin.myprofile
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import com.example.admin.myprofile.R.layout.activity_edu
+import com.example.admin.myprofile.R.layout.activity_work
 
 class MainActivity : AppCompatActivity() {
+    var photo:ImageView?=null
+    var eduButton:Button?=null
+    var workButton:Button?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.homescreen)
+        photo=findViewById(R.id.profilephoto)
+        photo?.setOnClickListener({
+            var clickIntent= Intent(this@MainActivity,ProfilePhoto::class.java)
+            startActivity(clickIntent)
+        })
+        eduButton=findViewById(R.id.edbutton)
+        eduButton?.setOnClickListener({
+            var click=Intent(this@MainActivity,Edu::class.java)
+            startActivity(click)
+        })
+
+        workButton=findViewById(R.id.wrkbutton)
+        workButton?.setOnClickListener({
+            var clickli=Intent(this@MainActivity,work::class.java)
+            startActivity(clickli)
+        })
     }
 }
